b2b9ff7da74bc28e2355b3425f339c44bf06fb86
Kaiyen Thompson <kthompson6300@gmail.com>
2020-07-09 10:25:40 -0400

    ready for review

--- src/main/java/com/codedifferently/stayready/Main.java
+++ src/main/java/com/codedifferently/stayready/Main.java
@@ -1,4 +1,33 @@
 package com.codedifferently.stayready;
-
 public class Main {
+    public static Double fromUSToEuro(Double USDollars){
+        return USDollars * (0.88/1.0);
+    }
+    public static Double fromEuroToUS(Double euros){
+        return euros * (1.13/1.0);
+    }
+    public static Double fromEuroToPound(Double euros){
+        return euros * (0.9/1.0);
+    }
+    public static Double fromPoundToRupee(Double pounds){
+        return pounds * (94.57/1.0);
+    }
+    public static Double fromRupeeToCanadianDollar(Double rupee){
+        return rupee * (0.018/1.0);
+    }
+    public static Double fromCanadianDollarToSingaporeDollar(Double canDollars){
+        return canDollars * (1.03/1.0);
+    }
+    public static Double fromSingaporeDollarToFranc(Double singDollars){
+        return singDollars * (0.67/1.0);
+    }
+    public static Double fromFrancToRinggit(Double francs){
+        return francs * (4.55/1.0);
+    }
+    public static Double fromRinggitToYen(Double ringgits){
+        return ringgits * (25.22/1.0);
+    }
+    public static Double fromYenToRenminbi(Double yens){
+        return yens * (0.065/1.0);
+    }
 }
--- src/test/java/com/codedifferently/stayready/MainTest.java
+++ src/test/java/com/codedifferently/stayready/MainTest.java
@@ -1,4 +1,109 @@
 package com.codedifferently.stayready;
-
+import org.junit.Assert;
+import org.junit.Test;
 public class MainTest {
+    //
+    // Exchange rates gotten from google conversion calculator
+    // Not Tariq's rates
+    //
+    @Test
+    public void usToEuroTest(){
+        // Given
+        Double input = 1.00; // representing number of US Dollars
+        Double expected = 0.88; // representing number of euros
+        // When
+        Double actual = Main.fromUSToEuro(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void euroToUSTest(){
+        // Given
+        Double input = 1.00; // // representing number of euros
+        Double expected = 1.13; // representing number of US Dollars
+        // When
+        Double actual = Main.fromEuroToUS(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void euroToPoundTest(){
+        // Given
+        Double input = 1.00; // representing number of euros
+        Double expected = 0.9; // representing number of pounds
+        // When
+        Double actual = Main.fromEuroToPound(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void poundToRupeeTest(){
+        // Given
+        Double input = 1.00; // representing number of pounds
+        Double expected = 94.57; // representing number of rupees
+        // When
+        Double actual = Main.fromPoundToRupee(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void rupeeToCanDollarsTest(){
+        // Given
+        Double input = 1.00; // representing number of rupees
+        Double expected = 0.018; // representing number of Canadian Dollars
+        // When
+        Double actual = Main.fromRupeeToCanadianDollar(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void canDollarsToSingDollarsTest(){
+        // Given
+        Double input = 1.00; // representing number of Canadian Dollars
+        Double expected = 1.03; // representing number of Singapore Dollars
+        // When
+        Double actual = Main.fromCanadianDollarToSingaporeDollar(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void singDollarsToFrancsTest(){
+        // Given
+        Double input = 1.00; // representing number of Singapore Dollars
+        Double expected = 0.67; // representing number of Swiss Francs
+        // When
+        Double actual = Main.fromSingaporeDollarToFranc(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void francsToRinggitTest(){
+        // Given
+        Double input = 1.00; // representing number of Swiss Francs
+        Double expected = 4.55; // representing number of Malaysian Ringgit
+        // When
+        Double actual = Main.fromFrancToRinggit(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void ringgitToYenTest(){
+        // Given
+        Double input = 1.00; // representing number of Malaysian Ringgit
+        Double expected = 25.22; // representing number of Japanese Yen
+        // When
+        Double actual = Main.fromRinggitToYen(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void yenToYuanRenminbiTest(){
+        // Given
+        Double input = 1.00; // representing number of Japanese Yen
+        Double expected = 0.065; // representing number of Chinese Yuan Renminbi
+        // When
+        Double actual = Main.fromYenToRenminbi(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 }
